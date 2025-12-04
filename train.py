e53b1506d7dae68c7072c408cc79760ecdb9b393
Vũ Hồng Thắm <vuhongtham@MacBook-Air-cua-Vu-4.local>
2025-12-04 16:38:49 +0700

    modify train.py

--- train.py
+++ train.py
@@ -1,7 +1,15 @@
 """Trains a chemprop model on a dataset."""
 
 from chemprop.train import chemprop_train
+from rdkit import RDLogger
+import warnings
+
+# Disable RDKit logging (C++ level)
+RDLogger.DisableLog('rdApp.*')
+
+# Disable Python warnings (optional, for other library warnings)
+warnings.filterwarnings("ignore")
 
 
-if __name__ == '__main__':
+if _name_ == '_main_':
     chemprop_train()
